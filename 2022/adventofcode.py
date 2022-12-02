ef5004f7ca5e11e4f091b2666cf6a5e3df54762f
Andreas van Cranenburgh <andreas@unstable.nl>
2022-12-02 20:28:00 +0100

    day 2

--- 2022/adventofcode.py
+++ 2022/adventofcode.py
@@ -13,13 +13,29 @@ sys.path.append('..')
 from common import main
 
 
-def day1a(s):
-	return max(sum(int(a) for a in elf.splitlines()) for elf in s.split('\n\n'))
+def day1(s):
+	sums = sorted(sum(int(a) for a in elf.splitlines())
+			for elf in s.split('\n\n'))
+	return sums[-1], sum(sums[-3:])
 
 
-def day1b(s):
-	return sum(sorted([sum(int(a) for a in elf.splitlines())
-			for elf in s.split('\n\n')])[-3:])
+def day2(s):
+	strategies = [line.split() for line in s.splitlines()]
+	strategies = [(ord(a) - 64, ord(b) - 87) for a, b in strategies]
+	rock, paper, scissors = lose, draw, _ = (1, 2, 3)
+	part1 = sum(b + (3 if a == b else
+		0 if (a == rock and b == scissors)
+			or (a == paper and b == rock)
+			or (a == scissors and b == paper)
+		else 6)
+		for a, b in strategies)
+	part2 = sum(
+		(paper if a == scissors else scissors if a == rock else rock)
+		if b == lose
+		else a + 3 if b == draw
+		else (scissors if a == paper else rock if a == scissors else paper) + 6
+		for a, b in strategies)
+	return part1, part2
 
 
 if __name__ == '__main__':
