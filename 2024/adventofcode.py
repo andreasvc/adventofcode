c66063b105f1689dd1f45d89e69597dceff45d95
Andreas van Cranenburgh <andreas@unstable.nl>
2024-12-26 14:33:52 +0100

    day 21-25

--- 2024/adventofcode.py
+++ 2024/adventofcode.py
@@ -590,20 +590,18 @@ def day19(s):
 
 def day20(s):
 	def find(start, end):
-		agenda = deque([(0, ) + start])
+		agenda = deque([start])
 		seen = {start: 0}
 		while agenda:
-			cost, x, y = agenda.popleft()
+			x, y = agenda.popleft()
 			if (x, y) == end:
 				return seen
-			if seen[x, y] < cost:
-				continue
 			for dx, dy in dirs:
-				nx, ny, ncost = x + dx, y + dy, cost + 1
+				nx, ny, ncost = x + dx, y + dy, seen[x, y] + 1
 				if 0 <= nx < xmax and 0 <= ny < ymax:
 					if grid[ny][nx] != '#':
 						if ncost < seen.get((nx, ny), 99999999):
-							agenda.append((ncost, nx, ny))
+							agenda.append((nx, ny))
 							seen[nx, ny] = ncost
 
 	grid = s.splitlines()
@@ -612,19 +610,230 @@ def day20(s):
 	end = max((line.find('E'), y) for y, line in enumerate(grid))
 	dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 	dist = find(start, end)
-	basecost = dist[end]
 	result1 = result2 = 0
 	for x1, y1 in dist:
 		for x2 in range(x1 - 20, x1 + 21):
-			for y2 in range(y1 - 20, y1 + 21):
+			yd = 20 - abs(x2 - x1)
+			for y2 in range(y1 - yd, y1 + yd + 1):
 				d = abs(x2 - x1) + abs(y2 - y1)
-				if 1 < d <= 20 and (x2, y2) in dist:
-					cost = dist[x1, y1] + d + (basecost - dist[x2, y2])
-					if basecost - cost >= 100:
+				if 2 <= d <= 20 and (x2, y2) in dist:
+					diff = dist[x1, y1] - dist[x2, y2] - d
+					if diff >= 100:
 						if d == 2:
 							result1 += 1
 						result2 += 1
 	return result1, result2
+
+
+def day21(s):
+	def find(code, pad):
+		x, y = pad['A']
+		bx, by = pad[' ']
+		result = ''
+		for c in code:
+			nx, ny = pad[c]
+			yxbad = ((x == bx and min(y, ny) <= by <= max(y, ny))
+					or (ny == by and min(x, nx) <= bx <= max(x, nx)))
+			xybad = ((y == by and min(x, nx) <= bx <= max(x, nx))
+					or (nx == bx and min(y, ny) <= by <= max(y, ny)))
+			if not yxbad and not xybad and y > ny:
+				result += (abs(x - nx) * ('<' if x > nx else '>')
+						+ abs(y - ny) * ('^' if y > ny else 'v') + 'A')
+			elif not yxbad:
+				result += (abs(y - ny) * ('^' if y > ny else 'v')
+						+ abs(x - nx) * ('<' if x > nx else '>') + 'A')
+			else:
+				result += (abs(x - nx) * ('<' if x > nx else '>')
+						+ abs(y - ny) * ('^' if y > ny else 'v') + 'A')
+			x, y = nx, ny
+		return result
+
+	def find2(code, n):
+		return sum(f(c0, c, n) for c0, c in zip('A' + code, code))
+
+	@cache
+	def f(c0, c, n):
+		x, y = dpad[c0]
+		nx, ny = dpad[c]
+		yxbad = ((x == bx and min(y, ny) <= by <= max(y, ny))
+				or (ny == by and min(x, nx) <= bx <= max(x, nx)))
+		xybad = ((y == by and min(x, nx) <= bx <= max(x, nx))
+				or (nx == bx and min(y, ny) <= by <= max(y, ny)))
+		if not yxbad and not xybad and n:
+			result1 = (abs(x - nx) * ('<' if x > nx else '>')
+					+ abs(y - ny) * ('^' if y > ny else 'v') + 'A')
+			result2 = (abs(y - ny) * ('^' if y > ny else 'v')
+					+ abs(x - nx) * ('<' if x > nx else '>') + 'A')
+			return min(
+					sum(f(a, b, n - 1)
+						for a, b in zip('A' + result1, result1)),
+					sum(f(a, b, n - 1)
+						for a, b in zip('A' + result2, result2)))
+		elif not yxbad:
+			result = (abs(y - ny) * ('^' if y > ny else 'v')
+					+ abs(x - nx) * ('<' if x > nx else '>') + 'A')
+		else:
+			result = (abs(x - nx) * ('<' if x > nx else '>')
+					+ abs(y - ny) * ('^' if y > ny else 'v') + 'A')
+		if n == 0:
+			return len(result)
+		return sum(f(a, b, n - 1)
+				for a, b in zip('A' + result, result))
+
+
+	npad = '789\n456\n123\n 0A'
+	dpad = ' ^A\n<v>'
+	npad, dpad = [{c: (x, y) for y, line in enumerate(pad.splitlines())
+			for x, c in enumerate(line)} for pad in [npad, dpad]]
+	bx, by = dpad[' ']
+	result1 = result2 = 0
+
+	for code in s.splitlines():
+		result1 += find2(find(code, npad), 1) * int(code[:-1])
+		result2 += find2(find(code, npad), 24) * int(code[:-1])
+	return result1, result2
+
+
+def day22(s):
+	result1 = 0
+	price = Counter()
+	for line in s.splitlines():
+		num = prev = int(line)
+		changes = []
+		seen = set()
+		for n in range(2000):
+			num ^= num << 6  # * 64
+			num &= 16777216 - 1  # num %= 16777216
+			num ^= num >> 5  # // 32
+			num &= 16777216 - 1  # num %= 16777216
+			num ^= num << 11 # * 2048
+			num &= 16777216 - 1  # num %= 16777216
+			num10 = num % 10
+			changes.append(num10 - prev)
+			prev = num10
+			if n >= 4:
+				seq = tuple(changes[-4:])
+				if seq not in seen:
+					seen.add(seq)
+					if num10:
+						price[seq] += num10
+		result1 += num
+	result2 = price.most_common()[0][1]
+	return result1, result2
+
+
+def day23(s):
+	conn = {}
+	for line in s.splitlines():
+		a, b = line.split('-')
+		if a not in conn:
+			conn[a] = set()
+		conn[a].add(b)
+		if b not in conn:
+			conn[b] = set()
+		conn[b].add(a)
+	result1 = set()
+	for a in conn:
+		if a[0] == 't':
+			for b, c in combinations(conn[a], 2):
+				if b in conn[c]:
+					result1.add(tuple(sorted((a, b, c))))
+
+	def bronkerbosch(r, p, x):
+		if not p and not x:
+			yield r
+		for v in list(p):
+			yield from bronkerbosch(r | {v}, p & conn[v], x & conn[v])
+			p.discard(v)
+			x.add(v)
+
+	maximumclique = max(bronkerbosch(set(), set(conn), set()), key=len)
+	result2 = ','.join(sorted(maximumclique))
+	return len(result1), result2
+
+
+def day24(s):
+	def run(wires, circuit):
+		todo = [True] * len(circuit)
+		change = True
+		while any(todo) and change:
+			change = False
+			for n, (a, op, b, _, out) in enumerate(circuit):
+				if not todo[n] or a not in wires or b not in wires:
+					continue
+				a, b = wires[a], wires[b]
+				if op == 'AND':
+					wires[out] = a and b
+				elif op == 'OR':
+					wires[out] = a or b
+				elif op == 'XOR':
+					wires[out] = (a or b) and not (a and b)
+				todo[n] = False
+				change = True
+		return wires
+
+	wires, circuit = s.split('\n\n')
+	wires = {a[:a.index(':')]: int(a[a.index(' ') + 1:])
+			for a in wires.splitlines()}
+	circuit = [line.split() for line in circuit.splitlines()]
+	newwires = run(wires.copy(), circuit)
+	zwires = [a for a in sorted(newwires) if a[0] == 'z']
+	result1 = sum((1 << n) * newwires[a] for n, a in enumerate(zwires))
+
+	result2 = []
+	switched = set()
+	xorred = {x for a, op, b, _, out in circuit if op == 'XOR'
+			for x in (a, b)}
+	orred = {x for a, op, b, _, out in circuit if op == 'OR'
+			for x in (a, b)}
+	anded = {x for a, op, b, _, out in circuit if op == 'AND'
+			for x in (a, b)}
+	hasinp = {}
+	for rule in circuit:
+		if rule[0] not in hasinp:
+			hasinp[rule[0]] = []
+		if rule[2] not in hasinp:
+			hasinp[rule[2]] = []
+		hasinp[rule[0]].append(rule)
+		hasinp[rule[2]].append(rule)
+	for n in range(45):
+		agenda = [f'x{n:02d}']
+		toprint = set()
+		for _ in range(3):
+			newagenda = []
+			for a in agenda:
+				toprint.update(tuple(x) for x in hasinp.get(a, ()))
+				for rule in hasinp.get(a, ()):
+					newagenda.append(rule[-1])
+			agenda = newagenda
+		for rule in sorted(toprint, key=lambda x: (
+				x[1] == 'OR', x[1] == 'AND', x[0][0] not in 'xy')):
+			print(' '.join(rule))
+		print()
+	for a, op, b, _, out in circuit:
+		if out[0] == 'z' and op != 'XOR' and out != 'z45':
+			switched.add(out)
+		if op == 'XOR' and out not in xorred | anded and out[0] != 'z':
+			switched.add(out)
+		if (op == 'XOR' and out[0] != 'z' and a[0] not in 'xy'
+				and b[0] not in 'xy'):
+			switched.add(out)
+		if op == 'AND' and out not in orred and {a, b} != {'x00', 'y00'}:
+			switched.add(out)
+		if op == 'OR' and out not in xorred | anded and out != 'z45':
+			switched.add(out)
+	result2 = ','.join(sorted(switched))
+	return result1, result2
+
+
+def day25(s):
+	keys, locks =  [[np.array([list(b) for b in a.splitlines()]) == '#'
+			for a in s.split('\n\n') if a[0] == c]
+			for c in '.#']
+	return sum(not (lock & key).any()
+			for lock in locks
+				for key in keys)
+
 
 if __name__ == '__main__':
 	main(globals())
